e55e7bfee523807dbf4e1a3af407c2f5aa4f6572
Ítalo Nascimento <italosousa42541@gmail.com>
2024-07-24 16:29:50 -0300

    atv

---
+++ Aula_06_JS/atv_01.js
@@ -0,0 +1,10 @@
+const pessoa = {
+    nome: 'Italo',
+    idade: 25,
+    cidade: 'Brasília - DF'
+};
+
+pessoa.profissao = "Desenvolvedor web"
+delete pessoa.idade;
+console.log(pessoa)
+
---
+++ Aula_06_JS/atv_02.js
@@ -0,0 +1,6 @@
+const pessoa = ["Pessoa1", "Pessoa2", "Pessoa3"]
+
+for (let key in pessoa) {
+    console.log(key + ": " + pessoa[key]);
+  }
+  
