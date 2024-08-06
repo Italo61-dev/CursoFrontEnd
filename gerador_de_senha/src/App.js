d33c847dc4464bb702c0d889eaa0a5c9c793bc12
Ítalo Nascimento <italosousa42541@gmail.com>
2024-08-06 15:40:46 -0300

    delete

--- gerador_de_senha/src/App.js
+++ gerador_de_senha/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [password, setPassword] = useState("")
+    const [copyText, setCopyText] = useState('copiar')
+
+    function generate() {
+      const characters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
+      const length = 12
+      let newPassword = ""
+      for (let i = 0; i < length; i++) {
+        const position = Math.floor(Math.random() * characters.length)
+        newPassword += characters[position]
+      }
+    }
+  
+  
   );
 }
 
