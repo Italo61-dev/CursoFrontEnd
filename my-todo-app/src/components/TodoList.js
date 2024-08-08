f0f200dfcbfa37b3e4af01cbf14e8bab8c288178
Ítalo Nascimento <italosousa42541@gmail.com>
2024-08-08 15:19:47 -0300

    upando

---
+++ my-todo-app/src/components/TodoList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function TodoList({todos, removeTodo}) {
+  return (
+    <div>
+        {todos.map((todo, index) => (
+
+          <div key={index} className='todo'>
+
+            <span>{todo.text}</span>
+
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </div>
+        ))}
+      </div>
+  )
+}
+
+export default TodoList
